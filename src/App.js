a7636b697eb4b97d2e3b3ee0db4d775055e8de64
amey <agnihotriamey19@gmail.com>
2024-12-18 22:28:54 +0530

    new code pushed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
 import axios from "axios";
 
 function App() {
   const [data, setData] = useState([]);
 
-  const handleClick = () => {
+  useEffect(() => {
     axios
       .get(
         "https://ta-backend-e8bxhkbhcjdcdgb5.westeurope-01.azurewebsites.net/customerInfo"
@@ -16,8 +14,8 @@ function App() {
         console.log(response.data && response.data.recordset);
         setData(response.data ? response.data.recordset : []);
       });
-  };
-  console.log(data);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">Hello</header>
