303da607a2df73ce2b4ac99de8d28234dc235294
amey <agnihotriamey19@gmail.com>
2024-12-18 22:23:51 +0530

    New cod Pushed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./App.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const handleClick = () => {
+    axios
+      .get(
+        "https://ta-backend-e8bxhkbhcjdcdgb5.westeurope-01.azurewebsites.net/customerInfo"
+      )
+      .then((response) => {
+        console.log(response.data && response.data.recordset);
+        setData(response.data ? response.data.recordset : []);
+      });
+  };
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">Hello</header>
+
+      {data.length > 0 &&
+        data.map((ele) => {
+          return <text href="#/action-1">{ele.firstName}</text>;
+        })}
     </div>
   );
 }
